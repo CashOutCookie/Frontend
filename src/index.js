257e5758b154792365e9f858144085c7de052af1
StingBolt <stingbolt1@gmail.com>
2021-02-21 20:48:29 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/default.css';
 import App from './App';
+import Register from './components/register';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const routing = (
+	<Router>
+		 <App />
+		<React.StrictMode>
+			<Switch>
+				<Route path="/register" component={Register} />
+			</Switch>
+		</React.StrictMode>
+	</Router>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
