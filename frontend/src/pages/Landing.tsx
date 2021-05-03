a0c2364c1b144d1e53affc2f6ed98c5acb370e36
Anshrk <anshrk.13@gmail.com>
2021-05-03 17:42:07 +0530

    reversed that dumb remake of the whole navbar

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,20 +1,12 @@
 import React, { FC } from 'react';
 import '../scss/Landing.scss';
+import Navbar from './Components/Navbar'
 
 export const Landing:FC = () => {
     return (
         <>
             <section>
-                <nav>
-                    <div className="logo">CC.</div>
-                    <div className="nav-contents">
-                        <span className="nav-content" id="HomeButton"><a href="/#">Home</a></span>
-                        <span className="nav-content" id="TutorialButton"><a href="/#">Tutorial</a></span>
-                        <span className="nav-content" id="PrizesButton"><a href="/#">Prizes</a></span>
-                        <span className="nav-content" id="SponsorButton"><a href="/#">Sponsor</a></span>
-                        <span className="nav-content" id="ContactButton"><a href="/#">Contact</a></span>
-                    </div>
-                </nav>
+                <Navbar />
                 <article>
                     <h1>Play games to earn real Prizes</h1>
                     <h2> Yes, its that simple.</h2>
