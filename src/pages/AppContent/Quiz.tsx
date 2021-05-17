a92cd5e27bf05da92177c992de4afff080a3bc08
Kunal Bagaria <kb24x7@gmail.com>
2021-05-17 22:46:40 +0530

    quiz complete! yayayayay

--- src/pages/AppContent/Quiz.tsx
+++ src/pages/AppContent/Quiz.tsx
@@ -1,31 +1,83 @@
 import React, { useState, useEffect } from 'react'
+import { useStopwatch } from 'react-timer-hook';
+import Countdown from 'react-countdown';
 import { decode } from 'html-entities';
 import Navbar from '../Components/Navbar'
 import SidebarIcons from '../Components/SidebarIcons'
 import Heading from './Heading'
 import Content from '../Content'
+import Animation from '../Components/Animations'
+import Loading from '../../animations/loading.json'
 import '../../scss/default.scss'
 import '../../scss/quiz.scss'
 
+const socket = new WebSocket(`ws://localhost:8000/quiz/`)
+
 const QuizContent = () => {
 
-    const [questionNumber, setQuestionNumber] = useState(0)
+    const [loading, setLoading] = useState(true)
     const [throttled, setThrottled] = useState(0)
     const [question, setQuestion] = useState('')
-    const [answer, setAnswer] = useState('')
     const [options, setOptions] = useState()
-    const [timer, setTimer] = useState(15)
+    const [message, setMessage] = useState('')
+    const [cookies, setCookies] = useState('')
+    const [done, setDone] = useState(false)
+    const { seconds, start, reset, pause } = useStopwatch({ autoStart: false })
+
+    console.log(seconds)
+
+    const sendAnswer = (answer) => {
+        socket.send(JSON.stringify({
+            "type": "quiz",
+            "answer": answer
+        }))
+    }
+
+    useEffect(() => {
+        if (seconds === 10) {
+            sendAnswer('')
+            pause()
+        }
+    }, [seconds, pause])
+
+    socket.onopen = () => {
+        socket.send(JSON.stringify({ 
+            "type": "auth",
+            "jwt": localStorage.getItem('token') 
+        }))
+    }
+    socket.onmessage = (event) => {
+        let data = JSON.parse(event.data)
+        console.log(data);
+        if (data.question) {
+            setQuestion(data.question)
+            setOptions(data.options)
+            setLoading(false)
+            reset()
+            start()
+        } else if (data.throttle) {
+            console.log('Throttled')
+            setThrottled(data.throttle)
+            setLoading(false)
+        } else if (data.message) {
+            setMessage(data.message)
+            setCookies(data.earned)
+            setDone(true)
+            setLoading(false)
+        }
+    }
 
     const OptionElement = () => {
-        if (questionNumber !== 0) {
+        if (!loading) {
             return(
                 <>
                     {// @ts-ignore
                     options.map(option => (
-                        <div onClick={() => {
-                            setAnswer(option)
+                        <div key={option} onClick={() => {
+                            pause()
+                            sendAnswer(option)
                         }} className="options">
-                            <p>{
+                            <p key={decode(option)}>{
                             // @ts-ignore
                                 option
                             }</p>
@@ -38,52 +90,44 @@ const QuizContent = () => {
         }
     }
 
-    useEffect(() => {
-        const socket = new WebSocket(`ws://localhost:8000/quiz/`)
-        socket.onopen = () => {
-            socket.send(JSON.stringify({ 
-                "type": "auth",
-                "jwt": localStorage.getItem('token') 
-            }))
-            if (answer) {
-                socket.send(JSON.stringify({
-                    "type": "quiz",
-                    "answer": answer
-                }))
-                setAnswer('')
-            }
-        }
-        socket.onmessage = (event) => {
-            let data = JSON.parse(event.data)
-            console.log(data);
-            if (data.question) {
-                setQuestion(data.question)
-                setOptions(data.options)
-                setQuestionNumber(questionNumber + 1)
-            } else if (data.throttle) {
-                console.log('Throttled')
-                setThrottled(data.throttle)
-            }
-        }
-    }, [setQuestion, setOptions, setAnswer, questionNumber, answer, ])
+        
 
 
-    if (throttled === 0) {
+    if (throttled === 0 && !loading && !done) {
         return(
             <div>
-                <h1 className="question">{`${questionNumber}.`} {decode(question)}</h1>
+                <h1 className="question">{decode(question)}</h1>
                 <div className="timer">
                     <div className="slider" style={{
-                        width: `${(timer/15) * 100}%`
+                        width: `${((10 - seconds) /10) * 100}%`
                     }}></div>
                 </div>
                 <OptionElement />
             </div>
         )
+    } else if (message) {
+        return(
+            <>
+                <div className="throttle">
+                    <p>{message}</p>
+                    <p>{cookies}</p>
+                </div>
+            </>
+        )
+    } else if (loading) {
+        return(
+            <div style={{ display: 'flex', alignItems: 'flex-start', marginLeft: "calc(40px + 2.2vw)", marginTop: "50px"}}>
+                <Animation json={Loading} height="calc(18px + 8vw)" width="calc(18px + 8vw)" />
+            </div>
+        )
     } else {
+        let secondsLeft = Number(throttled) * 1000
         return (
             <>
-                <p>{throttled}</p>
+                <div className="throttle">
+                    <p>You've already played quiz in the last 12 hours</p>
+                    <p>You can play again in: <Countdown date={Date.now() + secondsLeft} precision={0} /></p> 
+                </div>
             </>
         )
     }
